d4945a12cc597a96c5f57a19fe2f30e9cdc14030
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-06 18:53:29 +0900

    feat(util) : REGEX_PATTERN
    자동차 이름 입력 검증 정규식 작성

--- src/test/java/racingcar/util/REGEX_PATTERN.java
+++ src/test/java/racingcar/util/REGEX_PATTERN.java
@@ -8,8 +8,10 @@ public enum REGEX_PATTERN {
     SPACE(" "),
     COMMA(","),
     PURCHASE_VALIDATE_PATTERN("\\[[\\w가-힣]+-\\d+](,\\[[\\w가-힣]+-\\d+])*"),
-    YN_VALIDATE_PATTERN("[YN]");    // Y또는N, YN은 아님
-
+    YN_VALIDATE_PATTERN("[YN]"),   // Y또는N, YN은 아님
+    // 문자 5글자 이내가 1회 이상 반복, {1부터 5글자}만
+    CAR_NAMES_PATTERN("^([a-z|A-Z|가-힣]{1,5},)*([a-z|A-Z|가-힣]{1,5})$")
+;
     private final String regexPattern;
 
     REGEX_PATTERN(String regexPattern) {
