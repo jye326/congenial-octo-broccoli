b59b435dcb41f2ede72a767ff3319652ac61fa98
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-06 18:52:12 +0900

    feat(domain) : Car 추가

---
+++ src/test/java/racingcar/domain/Car.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int progress;
+
+    public Car(String name) {
+        this.name = name;
+        this.progress = 0;
+    }
+
+    public void runOrStop() {
+        // 나중에 상수 처리
+        if (Randoms.pickNumberInRange(0,9) >= 4) {
+            this.progress++;
+        }
+    }
+}
