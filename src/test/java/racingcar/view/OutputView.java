abf156912c7e32eb11fa528dc9d785246247e914
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-06 18:20:38 +0900

    feat(util, view) : base 코드 추가
    - ERROR_MESSAGE, OUTPUT_MESSAGE, REGEX_PATTERN
    - OutputView, UserInputView

---
+++ src/test/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import java.text.NumberFormat;
+import racingcar.util.OUTPUT_MESSAGE;
+
+public class OutputView {
+    // 일반 출력
+    public static void askMembership() {
+        System.out.println(OUTPUT_MESSAGE.openingComment);
+    }
+
+}
