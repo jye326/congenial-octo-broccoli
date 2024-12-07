1557884c598139b0e44dfb97e1ce3f53374014fb
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-07 19:47:10 +0900

    feat(util) : add constant

--- src/main/java/racingcar/util/OUTPUT_MESSAGE.java
+++ src/main/java/racingcar/util/OUTPUT_MESSAGE.java
@@ -3,7 +3,8 @@ package racingcar.util;
 public enum OUTPUT_MESSAGE {
     // 일반 정적 출력 문구 예시
     ASK_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    divider("===================================="),
+    ASK_TRY_TiMES("시도할 횟수는 몇 회인가요?"),
+
     doubleTab("\t\t"),
     // format 사용한 가변 출력 문구 예시
     PAY_AMOUNT("내실돈\t\t\t %s")
--- src/main/java/racingcar/util/REGEX_PATTERN.java
+++ src/main/java/racingcar/util/REGEX_PATTERN.java
@@ -10,13 +10,15 @@ public enum REGEX_PATTERN {
     PURCHASE_VALIDATE_PATTERN("\\[[\\w가-힣]+-\\d+](,\\[[\\w가-힣]+-\\d+])*"),
     YN_VALIDATE_PATTERN("[YN]"),   // Y또는N, YN은 아님
     // 문자 5글자 이내가 1회 이상 반복, {1부터 5글자}만
-    CAR_NAMES_PATTERN("^([a-z|A-Z|가-힣]{1,5},)*([a-z|A-Z|가-힣]{1,5})$")
+    CAR_NAMES_PATTERN("^([a-z|A-Z|가-힣]{1,5},)*([a-z|A-Z|가-힣]{1,5})$"),
+    ONLY_NUMBER("^[0-9]*"),
 ;
     private final String regexPattern;
 
     REGEX_PATTERN(String regexPattern) {
         this.regexPattern = regexPattern;
     }
+
     public String getRegexPattern() {
         return regexPattern;
     }
